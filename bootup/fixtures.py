09e4b385cd49b943c02d6d39c03e1972b9ab3db5
Val L33 <val@neekware.com>
2011-11-12 15:50:17 -0500

    Add BOOTUP prefix to fixture variable, up version and add history

--- bootup/fixtures.py
+++ bootup/fixtures.py
@@ -5,9 +5,9 @@ from django.core.management import call_command
 # lodd up fixtures right after the last installed app is loaded up
 def load_fixtures():
     """
-    Load initial fixtures located @ INITIAL_FIXTURES_DIRS defined in settings
+    Load initial fixtures located @ BOOTUP_INITIAL_FIXTURES_DIRS defined in settings
     """
-    path = getattr( settings, 'INITIAL_FIXTURES_DIRS', "")
+    path = getattr( settings, 'BOOTUP_INITIAL_FIXTURES_DIRS', "")
     if path and os.path.exists(path):
         for root, dirs, files in os.walk(path):
             for file in files:
